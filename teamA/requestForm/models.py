04529473c31fc0df52f4c4709ed8405adb84b05b
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-06-20 17:10:25 +0900

    [add]要望フォーム用のmodels.pyの記述

--- teamA/requestForm/models.py
+++ teamA/requestForm/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Request(models.Model):
+    request_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    mail = models.EmailField(max_length=100, null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+
+class RequestFunction(models.Model):
+    function_id = models.AutoField(primary_key=True)
+    request_id = models.ForeignKey(Request, on_delete=models.CASCADE)
+    function_name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(max_length=400, null=False, blank=False)
+
+
