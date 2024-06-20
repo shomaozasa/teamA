30bc25c525839beaa50217fa1c5a25e2f5394d85
UesugiDaiki <fko2247021@stu.o-hara.ac.jp>
2024-06-20 17:48:41 +0900

    [add]portfolioのルーティング追加

--- teamA/portfolio/views.py
+++ teamA/portfolio/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 
+# ホーム画面
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
+
+# 詳細画面
+def detail(request, pk):
+    template = loader.get_template('detail.html')
+    return HttpResponse(template.render({'pk': pk}, request))
