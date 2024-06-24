7372a5de532f19c0cbdaadd1dd4a1629d404c706
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-06-24 17:16:19 +0900

    [update]要望フォーム用のadmin.pyにモデルを追加

--- teamA/requestForm/admin.py
+++ teamA/requestForm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Request, RequestFunction
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Request)
+admin.site.register(RequestFunction)
