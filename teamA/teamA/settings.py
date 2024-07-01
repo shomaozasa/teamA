7c26cfa4f1d486c48cf84e0db0dcc4b884278f2f
UesugiDaiki <fko2247021@stu.o-hara.ac.jp>
2024-07-01 12:29:16 +0900

    Update settings.py

--- teamA/teamA/settings.py
+++ teamA/teamA/settings.py
@@ -101,6 +101,9 @@ DATABASES = {
     }
 }
 
+# Caches
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
