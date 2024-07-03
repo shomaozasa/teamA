9c2e0751d07f3318bcf5c7754dd7bdc8fea4549b
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-07-03 14:35:18 +0900

    [update]要望フォームのviews.pyを編集

--- teamA/requestForm/views.py
+++ teamA/requestForm/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from .models import Request, RequestFunction
+from .forms import RequestForm, RequestFunctionForm
+from django.forms import inlineformset_factory
 
-# 要望送信
 def request(request):
-    return redirect('index')
+    RequestFunctionFormSet = inlineformset_factory(
+        Request,
+        RequestFunction,
+        form=RequestFunctionForm,
+    )
+
+    if request.method == 'POST':
+        request_form = RequestForm(request.POST)
+        formset = RequestFunctionFormSet(request.POST)
+        if request_form.is_valid() and formset.is_valid():
+            request_instance = request_form.save()
+            formset.instance = request_instance
+            formset.save()
+            return redirect('index')
+    else:
+        request_form = RequestForm()
+        formset = RequestFunctionFormSet()
+
+    context = {
+        'request_form': request_form,
+        'formset': formset,
+    }
+    return render(request, 'index.html', context)
