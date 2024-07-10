f1196e4361dfbf190d6f22f99677f66c0ead7fd3
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-07-10 16:53:29 +0900

    [fix]要望フォームのviewsとmodelsの修正

--- teamA/requestForm/models.py
+++ teamA/requestForm/models.py
@@ -6,7 +6,7 @@ class Request(models.Model):
     request_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
     system_name = models.CharField(max_length=100, null=False, blank=False, default='default')
-    system_overview = models.CharField(null=False, blank=False, default='default')
+    system_overview = models.CharField(max_length=1000, null=False, blank=False, default='default')
     mail = models.EmailField(max_length=100, null=False, blank=False)
     date = models.DateField(auto_now_add=True, null=False, blank=False)
 
--- teamA/requestForm/views.py
+++ teamA/requestForm/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render, redirect
-from .models import Request, RequestFunction
-from .forms import RequestForm, RequestFunctionForm
-from django.forms import inlineformset_factory
+from .forms import RequestForm
 
 def request(request):
-    RequestFunctionFormSet = inlineformset_factory(
-        Request,
-        RequestFunction,
-        form=RequestFunctionForm,
-    )
-
     if request.method == 'POST':
-        request_form = RequestForm(request.POST)
-        formset = RequestFunctionFormSet(request.POST)
-        if request_form.is_valid() and formset.is_valid():
-            request_instance = request_form.save()
-            formset.instance = request_instance
-            formset.save()
+        form = RequestForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('index')
     else:
-        request_form = RequestForm()
-        formset = RequestFunctionFormSet()
-
-    context = {
-        'request_form': request_form,
-        'formset': formset,
-    }
-    return render(request, 'index.html', context)
+        form = RequestForm()
+    
+    return render(request, 'index.html', {'form': form})
