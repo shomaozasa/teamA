8e891b51d377865d1d2e69ab295bd93a0ebcd6d6
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-07-10 16:34:33 +0900

    [fix]要望フォームのformsとmodelsを修正

--- teamA/requestForm/forms.py
+++ teamA/requestForm/forms.py
@@ -1,26 +1,17 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from .models import Request, RequestFunction, User
+from .models import Request, User
 
 class RequestForm(forms.ModelForm):
     class Meta:
         model = Request
-        fields = ['name', 'system_name', 'mail']
+        fields = ['name', 'system_name', 'system_overview', 'mail']
         labels = {
             'name': '名前',
             'system_name': 'システム名',
+            'system_overview': 'システム概要',
             'mail': 'メールアドレス',
         }
         
-class RequestFunctionForm(forms.ModelForm):
-    class Meta:
-        model = RequestFunction
-        fields = ['function_name', 'description']
-        labels = {
-            'function_name': '機能名',
-            'description': '機能の説明',
-        }
-
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
--- teamA/requestForm/models.py
+++ teamA/requestForm/models.py
@@ -10,13 +10,8 @@ class Request(models.Model):
     request_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
     system_name = models.CharField(max_length=100, null=False, blank=False, default='default')
+    system_overview = models.CharField(null=False, blank=False, default='default')
     mail = models.EmailField(max_length=100, null=False, blank=False)
     date = models.DateField(auto_now_add=True, null=False, blank=False)
-
-class RequestFunction(models.Model):
-    function_id = models.AutoField(primary_key=True)
-    request_id = models.ForeignKey(Request, on_delete=models.CASCADE)
-    function_name = models.CharField(max_length=50, null=False, blank=False)
-    description = models.TextField(max_length=400, null=False, blank=False)
 
 
