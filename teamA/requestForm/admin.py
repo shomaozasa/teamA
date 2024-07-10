086219ce0264577f10a4553f26844db071daebe9
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-07-10 16:41:00 +0900

    [fix]要望フォームのadmin.pyの修正

--- teamA/requestForm/admin.py
+++ teamA/requestForm/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import User, Request, RequestFunction
+from .models import User, Request
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Request)
-admin.site.register(RequestFunction)
