9d39385ec0c14175c7af0f4b4fa441327ec53826
UesugiDaiki <fko2247021@stu.o-hara.ac.jp>
2024-07-11 11:59:55 +0900

    [update] DBのデータ表示とページネーション作成

--- teamA/portfolio/views.py
+++ teamA/portfolio/views.py
@@ -1,16 +1,34 @@
-from .models import Work, UsedLang, Image, Language
+from .models import Work, UsedLang, Image
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # ホーム画面
 def index(request, page=1):
+    # ページネーション用の変数
     display_num = 4
     page_num = page * display_num
-    work = Work.objects.order_by('-id').all()[page_num - display_num:page_num]
-    image = Image.objects.all()
+    on_each_side = 2
+    works_contents = []
 
+    # DBから作品と画像のデータ取得
+    all_works = Work.objects.order_by('-id').all()
+    works = all_works[page_num - display_num:page_num]
+    for work in works:
+        image_path = Image.objects.filter(work_id=work.id).first()
+        works_contents.append({
+            'work': work,
+            'image_path': image_path
+        })
+
+    # ページネーションリンク
+    data_page = Paginator(all_works, display_num)
+    data_p = data_page.get_page(page)
+    data_list = data_p.paginator.get_elided_page_range(page, on_each_side=on_each_side)
+    
     context = {
-        'work': work,
-        'image': image,
+        'works': works_contents,
+        'data_p': data_p,
+        'data_list': data_list,
     }
     return render(request, 'index.html', context)
 
