fbf0dcfdf69f11b566f64e785cfe86b1dc1c99f1
8311yua <tennnemu5473@docomo.ne.jp>
2024-07-16 12:11:04 +0900

    [update]送信後メッセージ

--- teamA/requestForm/views.py
+++ teamA/requestForm/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RequestForm
 from django.http import Http404
+from django.contrib import messages
 
 def request(request):
     # フォームの内容をDBに保存
@@ -8,8 +9,10 @@ def request(request):
         form = RequestForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.info(request, f'送信完了しました！')
             return redirect('index')
     else:
         raise Http404()
-
+    
+    messages.info(request, f'送信できませんでした。')
     return redirect('index')
