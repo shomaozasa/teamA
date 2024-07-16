205bb8e100415c3d9ca6624c194c09e9d5a4a7af
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-07-16 17:45:49 +0900

    [update]管理者ページでステータスを変更する際、わかりやすくなるように変更

--- teamA/requestForm/admin.py
+++ teamA/requestForm/admin.py
@@ -1,6 +1,19 @@
+from django import forms
 from django.contrib import admin
 from .models import Request, User
 # Register your models here.
+
+
+class RequestForm(forms.ModelForm):
+    class Meta:
+        model = Request
+        fields = '__all__'
+        widgets = {
+            'status': forms.RadioSelect
+        }
+
+class RequestAdmin(admin.ModelAdmin):
+    form = RequestForm
 
 admin.site.register(Request)
 admin.site.register(User)
--- teamA/requestForm/models.py
+++ teamA/requestForm/models.py
@@ -3,16 +3,23 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Request(models.Model):
+    STATUS_CHOICES = [
+        (0, '要望受付'),
+        (1, '制作途中'),
+        (2, '制作完了'),
+    ]
+    
     request_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
     system_name = models.CharField(max_length=100, null=False, blank=False, default='default')
     system_overview = models.CharField(max_length=1000, null=False, blank=False, default='default')
     mail = models.EmailField(max_length=100, null=False, blank=False)
     date = models.DateField(auto_now_add=True, null=False, blank=False)
-    status = models.IntegerField(default=0)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    
     def __str__(self):
         return self.system_name
-    
+
 class User(AbstractUser):
     pass
 
