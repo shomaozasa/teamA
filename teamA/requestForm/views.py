c7e9041a57cda3a7ec5f283d331113c08fbd5a10
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-08-20 14:29:45 +0900

    [update]DBに要望を登録した後、メールでその内容を送信できるように変更

--- teamA/requestForm/views.py
+++ teamA/requestForm/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RequestForm
 from django.http import Http404
 from django.contrib import messages
+from django.core.mail import send_mail
 
 def request(request):
     # フォームの内容をDBに保存
@@ -9,6 +10,30 @@ def request(request):
         form = RequestForm(request.POST)
         if form.is_valid():
             form.save()
+            
+            name = request.POST.get('name')
+            mail = request.POST.get('mail')
+            system_name = request.POST.get('system_name')
+            system_overview = request.POST.get('system_overview')
+
+            # メッセージの内容を作成
+            message = f"""
+            氏名: {name}
+            メールアドレス: {mail}
+            システム名: {system_name}
+            システム内容:
+            {system_overview}
+            """
+            
+            # 確認メールを送信
+            send_mail(
+                'システム要望依頼',  # 件名
+                message,  # メッセージ
+                'noreply.figcom@gmail.com',  # 送信元のメールアドレス
+                ['figcom@googlegroups.com'],  # 送信先のメールアドレスのリスト
+                fail_silently=False,
+            )
+            
             messages.info(request, f'送信が完了しました！')
             return redirect('index')
     else:
--- teamA/teamA/settings.py
+++ teamA/teamA/settings.py
@@ -151,3 +151,10 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "requestForm.User"
+
+DEFAULT_FROM_EMAIL = 'noreply.figcom@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'noreply.figcom@gmail.com'
+EMAIL_HOST_PASSWORD = 'wqfs ouxw hztf jhzz'
